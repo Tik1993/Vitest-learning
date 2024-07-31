06604c4faaa63ff84c9984cdc3722ab91292edb6
Ernest Lin <linchongtik@gmail.com>
2024-07-31 15:29:18 -0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Testing</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Counter from "./Counter";
+
+test("increments counter", () => {
+  const { getByText } = render(<Counter />);
+  const button = getByText("Increment");
+  const count = getByText(/count/i);
+
+  expect(count).toHaveTextContent("Count: 0");
+  fireEvent.click(button);
+  expect(count).toHaveTextContent("Count: -1");
+});
